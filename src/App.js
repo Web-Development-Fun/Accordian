57634abc392181c11c7af1176e50a71ac2ed278d
unknown <patidaranil0791@gmail.com>
2022-01-04 12:48:23 +0530

    Accordian

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Accordion from "./components/Accordion";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Header />
+      <Accordion />
+    </div>
+  );
+};
+
+export default App;
